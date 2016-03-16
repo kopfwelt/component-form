7e1b55b956cb2844dafdbd16c874e0e35f7beb86
Steve Sinnwell <s@hopeglory.com>
2016-03-16 19:44:21 +0100

    Update form-validate.js

--- scripts/form-validate.js
+++ scripts/form-validate.js
@@ -1,4 +1,13 @@
-// import parsley from 'parsley';
+const $ = require('jquery');
+window.jQuery = $;
+// require('parsleyjs/src/i18n/es.js');
+require('parsleyjs');
+// window.Parsley.setLocale('es');
 
 class FormValidated {
+	constructor() {
+		console.log('FormValidated');
+	}
 }
+
+module.exports = FormValidated;
